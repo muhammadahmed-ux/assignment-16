c50074399c11752be9720ba6b5171437b309ff7f
muhammadahmed-ux <ahmedjod321@gmail.com>
2025-07-23 17:58:12 -0700

    add files

---
+++ app.js
@@ -0,0 +1,457 @@
+
+// Chapter 1:Alerts
+
+// 1.
+// alert ("Hello Visitor");
+
+// 2.
+// alert ("Error! Please enter a valid password")
+
+// 3.
+// alert ("Welcome to JS Land...\n Happy Coding!")
+
+// 4.
+// alert ("Welcome to JS Land...")
+
+// alert("Happy Coding! \n Prevent this page from additional dialogs.")
+
+// 5.
+// console.log("Hello...I can run JS through my web browsers console");
+
+// 6
+// Done
+
+// 7
+// Done
+
+// chapter 2:variables for strings
+
+// 1.
+// var userName ="Areeb";
+// console.log("Areeb")
+
+// 2.
+// var myName ="Mohammad Areeb";
+// console.log("Mohammad Areeb")
+
+// 3.
+// var message="Hello World"
+// alert (message)
+
+// 4.
+// var message="Jhone Doe"
+//  alert (message)
+
+//  var message="15 Years old"
+//  alert (message)
+
+// var message="Certified Mobile Application Development"
+//  alert (message)
+
+// 5.
+// alert ( "PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// 6.
+// var email="mohammad.areeb1000@gmail.com"
+// alert (email)
+
+// 7.
+// var book=" I am trying to learn from the book A smarter way to learn javascript"
+// alert(book)
+
+// 8.
+// document.write("Yah! I can write HTML content through Javascript");
+
+// 9.
+// var design ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert (design)
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// chapter3:variables for numbers
+
+// 1.
+// var age = "I am 16 yeears old"
+// alert (age)
+
+// 2.
+// var time = "You have visited this site 14 times"
+// alert(time)
+
+// 3.
+// var birthyear = 2008;
+// document.write(`My birth year is ${birthyear}`);
+
+// 4.
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt(s)";
+// var quantity = 5;
+
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");
+
+// Chapter5:VARIABLE NAMES: LEGAL & ILLEGAL
+
+// 1.
+// var name = "Areeb", age = 16, city = "Karachi";
+
+// 2.
+//   5 Legal Variables:
+
+// var userName;
+// var _age;
+// var $price;
+// var totalMarks;
+// var user1;
+
+//  5 illegal Variables:
+
+// var 1user;        
+// var var;          
+// var user-name;    
+// var full name;    
+// var @email;   
+
+// 3.
+
+// document.write(variable names )
+// document.write()
+// document.write()
+// document.write()
+
+// Displaying heading and rules
+// document.write("<h1>Rules for naming JS variables</h1>");
+
+// document.write("<p>Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>$</b>, and <b>_</b>.</p>");
+
+// document.write("<p>Variables must begin with a <b>letter</b>, <b>$</b> or <b>_</b>.<br>For example: <b>$name</b>, <b>_name</b> or <b>name</b></p>");
+
+// document.write("<p>Variable names are case <b>sensitive</b>.</p>");
+
+// document.write("<p>Variable names should not be JS <b>keywords</b>.</p>");
+
+
+// chapter5 :MATH EXPRESSIONS
+
+// 1.
+
+// var num1 = 5;
+// var num2 = 7;
+
+// var sum = num1 + num2;
+
+// document.write("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+// 2.
+// Declare two numbers
+// let num1 = 20;
+// let num2 = 6;
+
+// // Addition
+// let sum = num1 + num2;
+// document.write("The sum of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+// // Subtraction
+// let difference = num1 - num2;
+// document.write("The difference of " + num1 + " and " + num2 + " is " + difference + "<br>");
+
+// // Multiplication
+// let product = num1 * num2;
+// document.write("The product of " + num1 + " and " + num2 + " is " + product + "<br>");
+
+// // Division
+// let quotient = num1 / num2;
+// document.write("The division of " + num1 + " by " + num2 + " is " + quotient + "<br>");
+
+// // Modulus
+// let remainder = num1 % num2;
+// document.write("The remainder when " + num1 + " is divided by " + num2 + " is " + remainder + "<br>");
+
+
+// 3.
+// a. Declare a variable
+// var number;
+
+// // b. Show value after declaration
+// document.write("Value after variable declaration is: " + number + "<br>");
+
+// // c. Initialize the variable with some number
+// number = 5;
+
+// // d. Show initial value
+// document.write("Initial value: " + number + "<br>");
+
+// // e. Increment the variable
+// number++;
+
+// // f. Show value after increment
+// document.write("Value after increment is: " + number + "<br>");
+
+// // g. Add 7 to the variable
+// number = number + 7;
+
+// // h. Show value after addition
+// document.write("Value after addition is: " + number + "<br>");
+
+// // i. Decrement the variable
+// number--;
+
+// // j. Show value after decrement
+// document.write("Value after decrement is: " + number + "<br>");
+
+// // k. Find remainder after dividing by 3
+// var remainder = number % 3;
+
+// // l. Show the remainder
+// document.write("The remainder is: " + remainder);
+
+
+// 4.
+// Store ticket price in a variable
+// var ticketPrice = 600;
+
+// // Calculate cost of 5 tickets
+// var totalCost = ticketPrice * 5;
+
+// // Display the result
+// document.write("Total cost to buy 5 tickets to a movie is " + totalCost + " PKR");
+
+
+// 5.
+// Define the number for which the table will be shown
+// var number = 4;
+
+// // Display heading
+// document.write("<h2>Multiplication Table of " + number + "</h2>");
+
+// // Generate and display the table from 1 to 10
+// for (var i = 1; i <= 10; i++) {
+//   document.write(number + " x " + i + " = " + (number * i) + "<br>");
+// }
+
+// 6.
+// a. Store Celsius temperature
+// var celsiusTemp = 30;
+
+// // b. Convert to Fahrenheit and display result
+// var fahrenheitResult = (celsiusTemp * 9/5) + 32;
+// document.write(celsiusTemp + "°C is " + fahrenheitResult + "°F<br>");
+
+// // c. Store Fahrenheit temperature
+// var fahrenheitTemp = 86;
+
+// // d. Convert to Celsius and display result
+// var celsiusResult = (fahrenheitTemp - 32) * 5/9;
+// document.write(fahrenheitTemp + "°F is " + celsiusResult + "°C");
+
+// 7.
+// a. Price of item 1
+// var item1Price = 650;
+
+// // b. Price of item 2
+// var item2Price = 100;
+
+// // c. Ordered quantity of item 1
+// var item1Qty = 3;
+
+// // d. Ordered quantity of item 2
+// var item2Qty = 7;
+
+// // e. Shipping charges
+// var shippingCharges = 100;
+
+// // Calculate total cost
+// var totalCost = (item1Price * item1Qty) + (item2Price * item2Qty) + shippingCharges;
+
+// // Display the receipt
+// document.write("<h2>Shopping Cart</h2>");
+// document.write("Price of item 1 is " + item1Price + "<br>");
+// document.write("Quantity of item 1 is " + item1Qty + "<br>");
+// document.write("Price of item 2 is " + item2Price + "<br>");
+// document.write("Quantity of item 2 is " + item2Qty + "<br>");
+// document.write("Shipping Charges " + shippingCharges + "<br><br>");
+// document.write("Total cost of your order is " + totalCost + " PKR");
+
+// 8.
+// Store total marks and marks obtained
+// var totalMarks = 500;
+// var marksObtained = 410;
+
+// // Calculate percentage
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// // Display the result
+// document.write("<h2>Marks Sheet</h2>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+// document.write("Percentage: " + percentage + "%");
+
+// 9.
+// Convert total currency to Pakistani Rupees in one expression
+// var totalPKR = (10 * 104.80) + (25 * 28);
+
+// // Display the result
+// document.write("<h2>Currency in PKR</h2>");
+// document.write("Total Currency in PKR: " + totalPKR);
+
+// 10.
+// Initialize a variable
+// var number = 10;
+
+// // Perform all calculations in a single expression
+// var result = ((number + 5) * 10) / 2;
+
+// // Display the result
+// document.write("<h2>Arithmetic Result</h2>");
+// document.write("Initial number: " + number + "<br>");
+// document.write("Result after calculations: " + result);
+
+// 11.
+// a. Store the current year
+// var currentYear = 2025;
+
+// // b. Store the birth year
+// var birthYear = 2008;
+
+// // c. Calculate two possible ages
+// var age1 = currentYear - birthYear;
+// var age2 = age1 - 1;
+
+// // Display the result
+// document.write("<h2>Age Calculator</h2>");
+// document.write("Current Year: " + currentYear + "<br>");
+// document.write("Birth Year: " + birthYear + "<br>");
+// document.write("They are either " + age2 + " or " + age1 + " years old.");
+
+// 12.
+// a. Store radius in a variable
+// var radius = 20;
+
+// // Define the value of π
+// var pi = 3.142;
+
+// // b. Calculate circumference: Circumference = 2 × π × r
+// var circumference = 2 * pi * radius;
+
+// // c. Calculate area: Area = π × r²
+// var area = pi * radius * radius;
+
+// // Display results
+// document.write("<h2>The Geometrizer</h2>");
+// document.write("Radius of the circle: " + radius + "<br>");
+// document.write("The circumference is " + circumference + "<br>");
+// document.write("The area is " + area);
+
+// 13.
+// a. Store favorite snack
+// var favoriteSnack = "Chocolate Chip Cookies";
+
+// // b. Store current age
+// var currentAge = 17;
+
+// // c. Store maximum age
+// var maxAge = 80;
+
+// // d. Store estimated amount per day
+// var snacksPerDay = 3;
+
+// // e. Calculate total snacks needed
+// var yearsRemaining = maxAge - currentAge;
+// var totalSnacks = yearsRemaining * 365 * snacksPerDay;
+
+// // Output the result
+// document.write("<h2>The Lifetime Supply Calculator</h2>");
+// document.write("Favorite Snack: " + favoriteSnack + "<br>");
+// document.write("Current Age: " + currentAge + "<br>");
+// document.write("Estimated Maximum Age: " + maxAge + "<br>");
+// document.write("Amount Per Day: " + snacksPerDay + "<br><br>");
+// document.write("You will need " + totalSnacks + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".");
+
+// chapter6:math expressions 2
+
+// 1.
+// Take a number in a variable
+// var num = 10;
+
+// document.write("<h2>Result</h2>");
+// document.write("The value of num is: " + num + "<br><br>");
+
+// // Pre-increment
+// document.write("The value of ++num is: " + (++num) + "<br>");
+// document.write("Now the value of num is: " + num + "<br><br>");
+
+// // Post-increment
+// document.write("The value of num++ is: " + (num++) + "<br>");
+// document.write("Now the value of num is: " + num + "<br><br>");
+
+// // Pre-decrement
+// document.write("The value of --num is: " + (--num) + "<br>");
+// document.write("Now the value of num is: " + num + "<br><br>");
+
+// // Post-decrement
+// document.write("The value of num-- is: " + (num--) + "<br>");
+// document.write("Now the value of num is: " + num);
+
+// 2.
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+// 3.
+// // Take input from user
+// var userName = prompt("Enter your name:");
+
+// // Greet the user
+// document.write("Hello, " + userName + "! Welcome to our website.");
+
+// 4.
+// not given
+
+
+// 5.
+// Take number input from the user
+// var userInput = prompt("Enter a number to display its multiplication table:");
+
+// // Convert input to number or use 5 as default if input is empty
+// var number = userInput ? Number(userInput) : 5;
+
+// // Display heading
+// document.write("<h2>Multiplication Table of " + number + "</h2>");
+
+// // Loop to display the table
+// for (var i = 1; i <= 10; i++) {
+//   document.write(number + " x " + i + " = " + (number * i) + "<br>");
+// }
+
+// 6.
+// a) Take subject names from user
+// var subject1 = prompt("Enter name of first subject:");
+// var subject2 = prompt("Enter name of second subject:");
+// var subject3 = prompt("Enter name of third subject:");
+
+// // b) Total marks for each subject
+// var totalMarksPerSubject = 100;
+
+// // c) & d) Take obtained marks for each subject
+// var marks1 = +prompt("Enter obtained marks for " + subject1 + ":");
+// var marks2 = +prompt("Enter obtained marks for " + subject2 + ":");
+// var marks3 = +prompt("Enter obtained marks for " + subject3 + ":");
+
+// // e) Calculate total and percentage
+// var totalMarks = totalMarksPerSubject * 3;
+// var totalObtained = marks1 + marks2 + marks3;
+// var percentage = (totalObtained / totalMarks) * 100;
+
+// // Display result in table
+// document.write("<h2>Result Sheet</h2>");
+// document.write("<table border='1' cellpadding='10'>");
+// document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+// document.write("<tr><td>" + subject1 + "</td><td>100</td><td>" + marks1 + "</td></tr>");
+// document.write("<tr><td>" + subject2 + "</td><td>100</td><td>" + marks2 + "</td></tr>");
+// document.write("<tr><td>" + subject3 + "</td><td>100</td><td>" + marks3 + "</td></tr>");
+// document.write("<tr><th colspan='2'>Total</th><th>" + totalObtained + " / " + totalMarks + "</th></tr>");
+// document.write("<tr><th colspan='2'>Percentage</th><th>" + percentage.toFixed(2) + "%</th></tr>");
+// document.write("</table>");
+
+
+
+// ---------complete------------ //
+
